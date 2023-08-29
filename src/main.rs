8bb90daa1ab220c0918c673c88a6c207a570aa2e
James Cuzella <james.cuzella@lyraphase.com>
2023-08-29 13:47:28 -0600

    Simple kvstore program example (modified)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let mut argv = std::env::args().skip(1);
+    let key = argv
+        .next()
+        .expect("No key passed!  Did you forget to pass args: key value");
+    let value = argv
+        .next()
+        .expect("No value passed! Did you forget to pass args: key value");
+    println!("key: {} = {}", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    let path = "/tmp/kvstore.db";
+    let result = std::fs::write(path, contents);
+    match result {
+        Ok(()) => {
+            println!("Successfully wrote to db file: {path}\n");
+            std::process::exit(0);
+        }
+        Err(e) => {
+            panic!("Error writing to db file: {path}\n{e}");
+        }
+    }
+}
+
 }
